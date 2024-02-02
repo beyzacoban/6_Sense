bf5ded66b34ef0b26c815c312f6b76d8f9ad7721
HasanTekbas <55557330+hasantkbs@users.noreply.github.com>
2024-02-02 20:16:28 +0300

    location update code

--- Location/LocationInfo.py
+++ Location/LocationInfo.py
@@ -1,7 +1,34 @@
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="Brave")
-location = geolocator.geocode("Istanbul")
-print(location.address)
-print((location.latitude, location.longitude))
-(40.7410861, -73.9896297241625)
-print(location.raw)
+from geopy.exc import GeocoderTimedOut
+import geocoder
+import time
+
+# GPS verilerini almak için kullanılacak bir fonksiyon
+def get_current_location():
+    try:
+        # Geocoder ile anlık konumu al
+        location = geocoder.ip('me').latlng
+        return location
+    except GeocoderTimedOut:
+        # Zaman aşımı hatası durumunda tekrar deneyin
+        return get_current_location()
+
+# GPS koordinatlarını adres bilgisine çevirmek için kullanılacak bir fonksiyon
+def get_address_from_coordinates(latitude, longitude):
+    geolocator = Nominatim(user_agent="Brave")
+    location = geolocator.reverse((latitude, longitude), language="en")
+    return location.address
+
+while True:
+    # Anlık konum bilgisi al
+    current_location = get_current_location()
+    print("Current Location:")
+    print("Latitude:", current_location[0])
+    print("Longitude:", current_location[1])
+
+    # GPS koordinatlarını adres bilgisine çevir
+    address = get_address_from_coordinates(current_location[0], current_location[1])
+    print("Address:", address)
+
+    # Belirli bir süre bekleyerek tekrar konum bilgisi alınmasını sağla
+    time.sleep(60)  # Örneğin, 60 saniye (1 dakika) aralıklarla konum almayı sağlar.
